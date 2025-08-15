5e726dc023000e807acdef501422897b8d216c4c
Teseo jaso <jasoteseo22@gmail.com>
2025-08-14 23:57:05 -0300

    VentanaCalculadora
    Quedo muy bien

---
+++ EjercicioVentanaCalculadora/Gestor.java
@@ -0,0 +1,13 @@
+package EjercicioVentanaCalculadora;
+
+public class Gestor {
+
+	public static void main(String[] args) {
+		
+		Ventana1 ven1 = new Ventana1();
+		ven1.setVisible(true);
+		
+		
+	}
+
+}
